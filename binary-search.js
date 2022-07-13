08e375958026d2b6f056359c6796b24626a15dd9
labor-day <shaffer.scott@yahoo.com>
2022-07-13 15:13:24 -0600

    complete

--- binary-search.js
+++ binary-search.js
@@ -1,27 +1,55 @@
 function linearSearch (arr, target) {
 
   // Can you solve this in one line?
+  //return arr.indexOf(target);
 
+  // return arr.reduce(
+  //   (targetIndex, element, index) => {
+  //     if (element === target) {
+  //       return index;
+  //     } else {
+  //       return targetIndex;
+  //     }
+  //   }, -1
+  // );
+
+  for (let i = 0; i < arr.length; i++) {if (arr[i] === target) {return i}} return -1;
 };
 
 function binarySearch(arr, target) {
 
   // Set integers pointing to the high and low range of possible indices
+  let low = 0;
+  let hi = arr.length - 1;
 
   // While high and low indices do not overlap...
+  while (hi >= low && low <= hi) {
 
     // Find the midpoint between high and low indices
+     let mid = Math.floor((hi + low) / 2);
 
     // Compare the target value to the midpoint value
-
-    // If the target equals the midpoint...
+    if (arr[mid] === target) {
+      // If the target equals the midpoint...
       // Return the midpoint index
-    // If the target is higher than the midpoint...
+      return mid;
+    }
+
+    if (arr[mid] < target) {
+      // If the target is higher than the midpoint...
       // Move the low pointer to midpoint + 1
-    // If the target is less than the midpoint...
+      low = mid + 1;
+    }
+
+    if (arr[mid] > target){
+      // If the target is less than the midpoint...
       // Move the high pointer to midpoint - 1
+      hi = mid - 1;
+    }
+  }
 
   // Return -1 if the loop exits with overlapping pointers
+  return -1;
 
 }
 
